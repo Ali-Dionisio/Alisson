b683a62d589bdbf563e9a9d0e352cc149fafcc0a
Alisson <alisson.a-work@outlook.com>
2026-01-08 09:35:32 -0300

    corrigindo nomenclatura

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
